65e6dd457adff0e4c6a30f796130d305bf8e0590
Serhii Shevchenko <zh.send@gmail.com>
2024-10-10 05:37:46 +0700

    Different `swift-syntax` for different Swift version

--- Package@swift-6.0.swift
+++ Package@swift-6.0.swift
@@ -17,7 +17,7 @@ let package = Package(
 		),
 	],
 	dependencies: [
-		.package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0-latest"),
+		.package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0"),
 	],
 	targets: [
 		/// Macro plugin target
