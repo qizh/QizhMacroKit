8fb89d064289fae2785aff19e8ccf339aaf3fca9
Serhii Shevchenko <zh.send@gmail.com>
2025-05-31 18:38:22 +0700

    Add `Diagnostic` shortcuts

--- Sources/QizhMacroKitMacros/QizhMacroGeneratorDiagnostic.swift
+++ Sources/QizhMacroKitMacros/QizhMacroGeneratorDiagnostic.swift
@@ -12,9 +12,84 @@ public struct QizhMacroGeneratorDiagnostic: DiagnosticMessage {
 	public let diagnosticID: MessageID
 	public let severity: DiagnosticSeverity
 
-	public init(_ message: String, severity: DiagnosticSeverity = .error) {
+	public init(
+		message: String,
+		id messageID: String,
+		severity: DiagnosticSeverity
+	) {
 		self.message = message
-		self.diagnosticID = MessageID(domain: "QizhKitMacros", id: "InvalidUsage")
+		self.diagnosticID = MessageID(domain: "QizhKitMacros", id: messageID)
 		self.severity = severity
+	}
+	
+	/// Error Diagnostic
+	@inlinable public init(
+		_ message: String,
+		id messageID: String = "InvalidUsage",
+		severity: DiagnosticSeverity = .error
+	) {
+		self.init(message: message, id: "InvalidUsage", severity: .error)
+	}
+}
+
+extension Diagnostic {
+	public static func error(
+		node: some SyntaxProtocol,
+		message: String,
+		id messageID: String
+	) -> Diagnostic {
+		Diagnostic(
+			node: node,
+			message: QizhMacroGeneratorDiagnostic(
+				message: message,
+				id: messageID,
+				severity: .error
+			)
+		)
+	}
+	
+	public static func warning(
+		node: some SyntaxProtocol,
+		message: String,
+		id messageID: String
+	) -> Diagnostic {
+		Diagnostic(
+			node: node,
+			message: QizhMacroGeneratorDiagnostic(
+				message: message,
+				id: messageID,
+				severity: .warning
+			)
+		)
+	}
+	
+	public static func note(
+		node: some SyntaxProtocol,
+		message: String,
+		id messageID: String
+	) -> Diagnostic {
+		Diagnostic(
+			node: node,
+			message: QizhMacroGeneratorDiagnostic(
+				message: message,
+				id: messageID,
+				severity: .note
+			)
+		)
+	}
+	
+	public static func remark(
+		node: some SyntaxProtocol,
+		message: String,
+		id messageID: String
+	) -> Diagnostic {
+		Diagnostic(
+			node: node,
+			message: QizhMacroGeneratorDiagnostic(
+				message: message,
+				id: messageID,
+				severity: .remark
+			)
+		)
 	}
 }
