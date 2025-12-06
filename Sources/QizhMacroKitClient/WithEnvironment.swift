10872c84159ead1d6d448af7916b284f2dc47d48
Serhii Shevchenko <zh.send@gmail.com>
2025-12-06 15:57:57 +0700

    Add `WithEnvironment` SwiftUI view example
    - [x] Introduces `ViewUsingWithEnvironment`, demonstrating usage of `#WithEnvironment` macro with `Foo` and `Bar` observable objects in SwiftUI.

---
+++ Sources/QizhMacroKitClient/WithEnvironment.swift
@@ -0,0 +1,39 @@
+//
+//  SwiftUIView.swift
+//  QizhMacroKit
+//
+//  Created by Serhii Shevchenko on 06.12.2025.
+//
+
+import SwiftUI
+import QizhMacroKit
+
+class Foo: ObservableObject {
+	@Published var message: String = "Hello, World!"
+	
+	init(message: String) {
+		self.message = message
+	}
+}
+
+@Observable
+class Bar {
+	var text: String = "Hello, World!"
+	
+	init(text: String) {
+		self.text = text
+	}
+}
+
+struct ViewUsingWithEnvironment: View {
+	let int = 1
+	let str = "2"
+	
+    var body: some View {
+		#WithEnvironment {
+			@EnvironmentObject var foo: Foo
+			@Environment(Bar.self) var bar
+		} {
+			Text("Hello, World! \(int) \(str) \(foo.message) \(bar.text)")
+		}
+    }
