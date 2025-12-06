5a96a11f004c765f9c90f6fff3e96374e64c085d
Serhii <zh.send@gmail.com>
2025-12-06 09:41:08 +0700

    Apply suggestion from @qizh

--- Sources/QizhMacroKit/WithEnvironment.swift
+++ Sources/QizhMacroKit/WithEnvironment.swift
@@ -1,3 +1,10 @@
+//
+//  WithEnvironment.swift
+//  QizhMacroKit
+//
+//  Created by Qizh in December 2025.
+//
+
 /// Generates a helper wrapper view that injects environment values into the attached view expression.
 ///
 /// The macro accepts an optional name and a closure with plain variable declarations. Each declaration
